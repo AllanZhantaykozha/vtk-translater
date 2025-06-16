9f8d0e452f4623da012cdaaeab6b3ac7972fd6ff
Allan Zhantaykozha <theallan765@gmail.com>
2025-06-17 00:11:53 +0500

    sadsd

--- src/hooks/useTranslate.tsx
+++ src/hooks/useTranslate.tsx
@@ -11,37 +11,6 @@ interface TranslateOptions {
   targetLang: string;
 }
 
-export async function handler(event: any) {
-  const authKey = "231849e1-6c06-4751-b3d6-a762caadcc79:fx";
-
-  try {
-    const res = await fetch("https://api-free.deepl.com/v2/translate", {
-      method: "POST",
-      headers: {
-        Authorization: `DeepL-Auth-Key ${authKey}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: event.body,
-    });
-
-    const data = await res.json();
-
-    return {
-      statusCode: res.status,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Ошибка на сервере" }),
-    };
-  }
-}
-
 export const useTranslate = () => {
   const [result, setResult] = useState<TranslationResult>({
     translatedText: null,
